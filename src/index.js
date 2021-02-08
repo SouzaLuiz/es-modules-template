ba4a8000927f3379600c2ebc31e71ee663fc968b
Luiz Henrique Souza <henryque.souz4@gmail.com>
2021-02-08 10:12:50 -0400

    feat: add esm lib

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ app.get('/', (req, res) => {
   return res.json({ ok: true })
 })
 
-app.listen(process.env.PORT || 3333)
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server is starter`)
+})
